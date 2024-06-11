c356dc1311f69ad4ee7b81630b94091d40b37959
Miracle Ibharokhonre <miracleosemudiahen@hotmail.com>
2024-06-11 15:32:57 +0100

    solution

---
+++ README.md
@@ -0,0 +1,53 @@
+# Frontend Mentor - Job listings with filtering solution
+
+This is a solution to the [Job listings with filtering challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/job-listings-with-filtering-ivstIPCt). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+
+
+## Table of contents
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Filter job listings based on the categories
+
+
+### Screenshot
+![Desktop View](images/desktop.png)
+
+![Mobile View](images/mobile.png)
+
+### Links
+- Solution URL: [Click to view Solution](https://www.frontendmentor.io/solutions/time-tracking-dashboard-solution-using-css-grid-MX_ntr65HI)
+- Live Site URL: [Click to view Live Site](https://coded-by-mj.github.io/Front-end-Mentor-Challenge15/)
+
+
+## My process
+
+### Built with
+- Semantic HTML5 markup
+- CSS 
+- Flexbox
+- JQuery
+- API
+- JSON
+- Mobile First Workflow
+
+
+
+## Author
+- Frontend Mentor - [@Coded-by-MJ](https://www.frontendmentor.io/profile/Coded-by-MJ)
+- Twitter - [@MiracleJustice_](https://twitter.com/miraclejustice_)
---
+++ script.js
@@ -0,0 +1,146 @@
+
+
+
+const filterArray = [];
+
+
+const getAllJobs = async ()  => {
+    const res = await fetch("data.json");
+    const data = await res.json();
+
+   showAllJobs(data);
+}
+
+
+const showAllJobs = (data) => {
+
+const listingHTML = data.map((card) => {
+    const {company, logo, position, featured, postedAt, contract, location} = card;
+    const newJob = card.new;
+    const skills = [card.role, card.level, ...card.tools, ...card.languages];
+    const skillsButtons = skills.map((skill) => `<button type="button" onclick="addToFilter(event)" class="skill-btn btn" value="${skill}" >${skill}</button>`);
+
+     return `
+        <div class="card ${newJob && featured ? "hl" : ''}" data-skills="${skills.join(' ')}">
+                <div class="job-container">
+                    <img class="logo" src="${logo}" alt="${company}"></img>
+
+                   <div class="job-content">
+                        <div class="job-info">
+                              <h3>${company}</h3>
+                              <div class="badge ${newJob ? 'new' : ''}"> ${newJob ? 'NEW!' : ''} </div>
+                              <div class="badge ${featured ? 'featured' : ''}"> ${featured ? 'FEATURED' : ''}</div>
+                        </div>
+
+                        <h3 class="job-title">${position}</h3>
+
+                        <div class="job-details">
+                               <span>${postedAt}</span> 
+                               <span>${contract}</span> 
+                               <span>${location}</span>
+                        </div>
+                        
+                     </div> 
+            
+
+                </div>
+
+                <div class="skills-wrapper">
+                    ${skillsButtons.join('')}
+                </div>
+        </div>
+        `;
+     }).join('');
+
+ $(".listing-wrapper").html(listingHTML);
+}
+
+
+
+
+
+
+
+const  addToFilter = (e) => {
+    const skillValue = $(e.target).val();
+
+    if(filterArray.indexOf(skillValue) > -1) {
+        return;  
+    }else{
+        filterArray.push(skillValue);
+        createCatergory(filterArray);
+    }
+
+}
+
+
+const removeFromFilter = (e) => {
+    const skillValue = $(e.target).val();
+    const skillIndex =  filterArray.indexOf(skillValue);
+    filterArray.splice(skillIndex, 1);
+    createCatergory(filterArray);
+}
+
+
+
+const clearFilter = () =>{
+    filterArray.length = 0;
+    createCatergory(filterArray);
+}
+
+
+const createCatergory = (arr) => {
+    if(arr.length === 0) {
+        getAllJobs();
+        $('.filter-wrapper').removeClass('show');
+        return;
+    }
+
+    $('.filter-wrapper').addClass('show');
+    showOrHideCard(arr);
+
+ const categoryHTML = arr.map((val) =>{
+    return `
+      <div class="catergory">
+        <span>${val}</span>
+        <button type="button" onclick="removeFromFilter(event)" value="${val}" class="remove btn"></button>
+      </div>
+    `
+  }).join('');
+
+  $(".catergory-wrap").html(categoryHTML);
+
+}
+
+
+
+
+const showOrHideCard = (arr) => {
+
+    $('.card').each((index, card) => {
+        const cardSkills = $(card).data("skills");
+        let showCard = false;
+
+        arr.forEach(skill => {
+            if (cardSkills.includes(skill)) {
+                showCard = true;
+                return false; 
+            }
+        });
+
+        if (showCard) {
+            $(card).show(); 
+        } else {
+            $(card).hide(); 
+        }
+    });
+
+}
+
+
+
+
+
+$('.clearAll').click(clearFilter);
+
+getAllJobs();
